7790507df63e27ba9b21cd823efbf03125b63028
Cabrera Hammer <hcabrera4@utmachala.edu.ec>
2026-05-31 19:47:11 -0500

    agregar menú principal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Sistema de Pedidos UTMACH");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SISTEMA DE PEDIDOS");
+        Console.WriteLine("1. Agregar pedido");
+        Console.WriteLine("2. Salir");
+    }
+}
