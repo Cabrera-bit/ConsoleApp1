9f475edb5adc2e4dec896633ef060d46186f4e59
Cabrera Hammer <hcabrera4@utmachala.edu.ec>
2026-05-31 20:33:53 -0500

    feat: actualizar mensaje principal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Sistema de Pedidos UTMACH");
+        Console.WriteLine("Sistema de Pedidos UTMACH v1.1");
         Console.WriteLine("1. Agregar pedido");
         Console.WriteLine("2. Salir");
     }
